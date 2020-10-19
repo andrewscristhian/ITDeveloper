efcb2624a351ad0e8e43f9feccad9c2e55e15a8c
andrewscristhian <andrewsfogo@gmail.com>
2020-10-19 17:53:51 -0300

    Views CRUD de Cid ajustadas - Aula 162

--- src/Curso.ITDeveloper.Mvc/Controllers/CidController.cs
+++ src/Curso.ITDeveloper.Mvc/Controllers/CidController.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace Curso.ITDeveloper.Mvc.Controllers
 {
@@ -25,12 +27,37 @@ namespace Curso.ITDeveloper.Mvc.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? pagina, string ordenacao, string stringBusca)
         {
-            return View(await _context.Cid.AsNoTracking()
-                .Where(c => c.CidInternalId < 1001)
-                .OrderBy(o => o.CidInternalId)
-                .ToListAsync());
+
+            const int itensPorPagina = 8;
+            int numeroPagina = (pagina ?? 1);
+
+            ViewData["ordenacao"] = ordenacao;
+            ViewData["filtroAtual"] = stringBusca;
+
+            var cids = from c in _context.Cid select c;
+
+            if (!string.IsNullOrEmpty(stringBusca)) cids = cids.Where(s => s.Codigo.Contains(stringBusca) || s.Diagnostico.Contains(stringBusca));
+
+            ViewData["OrderByInternalId"] = string.IsNullOrEmpty(ordenacao) ? "CidInternalId_desc" : "";
+            ViewData["OrderByCodigo"] = ordenacao == "Codigo" ? "Codigo_desc" : "Codigo";
+            ViewData["OrderByDiagnostico"] = ordenacao == "Diagnostico" ? "Diagnostico_desc" : "Diagnostico";
+
+            if (string.IsNullOrEmpty(ordenacao)) ordenacao = "CidInternalId";
+
+            if (ordenacao.EndsWith("_desc"))
+            {
+                ordenacao = ordenacao.Substring(0, ordenacao.Length - 5);
+                cids = cids.OrderByDescending(x => EF.Property<object>(x, ordenacao));
+            }
+            else
+            {
+                cids = cids.OrderBy(x => EF.Property<object>(x, ordenacao));
+            }
+
+            return View(await cids.AsNoTracking()
+                .ToPagedListAsync(numeroPagina, itensPorPagina));
         }
 
         public IActionResult ArquivoInvalido()
@@ -39,30 +66,31 @@ namespace Curso.ITDeveloper.Mvc.Controllers
             return View();
         }
 
+
         [HttpPost]
         public async Task<IActionResult> ImportCid(IFormFile file, [FromServices] IWebHostEnvironment webHostEnvironment)
         {
             // DRY
             if (!ArquivoValido.EhValido(file, "Cid.Csv")) return RedirectToAction("ArquivoInvalido"); // DELEGUEI
 
-            var filePath = $"{webHostEnvironment.WebRootPath}\\importFiles\\{file.FileName}";
+            var filePah = $"{webHostEnvironment.WebRootPath}\\importFiles\\{file.FileName}";
 
-            CopiarArquivo.Copiar(file, filePath); // DELEGUEI
+            CopiarArquivo.Copiar(file, filePah); // Deleguei
 
-            int k = 0; // cabecalho
+            int k = 0;
             string line;
 
             List<Cid> cids = new List<Cid>();
-            Encoding encodingPage = Encoding.GetEncoding(1252); // Codigo Formato ANSI
+            Encoding encodingPage = Encoding.GetEncoding(1252);
             bool detectEncoding = false;
 
-            using (var fs = System.IO.File.OpenRead(filePath))
+            using (var fs = System.IO.File.OpenRead(filePah))
             using (var stream = new StreamReader(fs, encoding: encodingPage, detectEncoding))
                 while ((line = stream.ReadLine()) != null)
                 {
                     string[] parts = line.Split(";");
                     // cidinternalid, codigo, diagnostico  (os campos que vem no cabecalho do .csv)
-                    if (k > 0) // Pular Cabecalho (Indice 0)
+                    if (k > 0) // Pular Cabechalho
                     {
                         if (!_context.Cid.Any(e => e.CidInternalId == int.Parse(parts[0])))
                         {
@@ -84,6 +112,105 @@ namespace Curso.ITDeveloper.Mvc.Controllers
             }
 
             return RedirectToAction(nameof(Index));
+        }
+
+        // CRUD Aqui
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+
+            var cid = await _context.Cid.FirstOrDefaultAsync(m => m.Id == id);
+            if (cid == null) return NotFound();
+
+            return View(cid);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Cid cid)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(cid);
+                    await _context.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cid);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var cid = await _context.Cid.FindAsync(id);
+            if (cid == null) return NotFound();
+
+            return View(cid);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Cid cid)
+        {
+            if (id != cid.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cid);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CidExists(cid.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cid);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var cid = await _context.Cid.FirstOrDefaultAsync(m => m.Id == id);
+            if (cid == null) NotFound();
+
+            return View(cid);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var cid = await _context.Cid.FindAsync(id);
+            _context.Cid.Remove(cid);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CidExists(Guid id)
+        {
+            return _context.Cid.Any(x => x.Id == id);
         }
     }
 }
